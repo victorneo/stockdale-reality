b97c6dd38132886b3212118a70ffdf8cf028776e
Victor Neo <icyisamu@gmail.com>
2018-10-28 11:54:30 +0800

    Initial prototype with asyncio (#2)
    * Initial prototype with asyncio
    
    * Flake8 cleanup

---
+++ app.py
@@ -0,0 +1,29 @@
+import importlib
+import aiohttp
+import asyncio
+
+
+def import_source(s_name):
+    dot_index = s_name.rfind('.')
+    class_name = s_name[dot_index+1:]
+    source_module = importlib.import_module('sources.'+s_name[:dot_index])
+    return getattr(source_module, class_name)
+
+
+async def main(sources, configs):
+    source_classes = []
+    for s_name in sources:
+        source_classes.append(import_source(s_name))
+
+    async with aiohttp.ClientSession() as session:
+        for idx, s in enumerate(source_classes):
+            si = s(session, **configs[idx])
+            await si.fetch()
+            await si.render()
+
+
+if __name__ == '__main__':
+    sources = ['httpcodesource.HttpCodeSource',
+               'httpcodesource.HttpCodeSource']
+    configs = [{'url': 'http://python.org'}, {'url': 'http://google.com'}]
+    asyncio.run(main(sources, configs))
